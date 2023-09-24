7f0d7717f30619689b8c496e57f567d511a84e97
Denton  Troy Ryan <dentontr@ug222.eecg.toronto.edu>
2023-09-24 15:35:42 -0400

    retab

--- circuit.h
+++ circuit.h
@@ -6,53 +6,51 @@
 #include <sstream>
 using namespace std;
 class connection {
-	public:
-		int x0 = 0, y0 = 0, p0 = 0, x1 = 0, y1 = 0, p1 = 0;
-		connection(int x_0, int y_0, int p_0, int x_1, int y_1, int p_1) {
-			x0 = x_0;
-			y0 = y_0;
-			p0 = p_0;
-			x1 = x_1;
-			y1 = y_1;
-			p1 = p_1;
-		}
+  public:
+    int x0 = 0, y0 = 0, p0 = 0, x1 = 0, y1 = 0, p1 = 0;
+    connection(int x_0, int y_0, int p_0, int x_1, int y_1, int p_1) {
+      x0 = x_0;
+      y0 = y_0;
+      p0 = p_0;
+      x1 = x_1;
+      y1 = y_1;
+      p1 = p_1;
+    }
 
-		string to_string() {
-			ostringstream outstring;
-			outstring << "x0: " << x0 << " y0: " << y0 << " p0: " << p0 << " x1: " << x1 << " y1: " << y1 << " p1: " << p1;
-			return outstring.str();
-		}
+    string to_string() {
+      ostringstream outstring;
+      outstring << "x0: " << x0 << " y0: " << y0 << " p0: " << p0 << " x1: " << x1 << " y1: " << y1 << " p1: " << p1;
+      return outstring.str();
+    }
 };
 
 class circuit {
-	public:
-		int grid_size, tracks_per_channel;
-		list<connection*> conns;
+  public:
+    int grid_size, tracks_per_channel;
+    list<connection*> conns;
 
-		circuit(int gs, int tpc) {
-			grid_size = gs;
-			tracks_per_channel = tpc;
-		}
-		
-		~circuit() {
-			for (auto* conn : conns){
-				delete(conn);
-			}
-		}
+    circuit(int gs, int tpc) {
+      grid_size = gs;
+      tracks_per_channel = tpc;
+    }
 
+    ~circuit() {
+      for (auto* conn : conns){
+        delete(conn);
+      }
+    }
 
-		void add_connection(connection* conn) {
-			conns.push_back(conn);
-		}
+    void add_connection(connection* conn) {
+      conns.push_back(conn);
+    }
 
-		string dump_connections() {
-			ostringstream outstring;
-			for (auto* conn : conns){
-				outstring << conn->to_string() << endl;
-			}
-			
+    string dump_connections() {
+      ostringstream outstring;
+      for (auto* conn : conns){
+        outstring << conn->to_string() << endl;
+      }
 
-			return outstring.str();
-		}
+      return outstring.str();
+    }
 };
 #endif
--- main.cpp
+++ main.cpp
@@ -11,96 +11,96 @@ using namespace std;
 
 
 circuit* read_input(string file) {
-	string line;
-	int grid;
-	int tracks_per_channel;
-	circuit* circ = nullptr;
-	ifstream infile (file);
-	spdlog::debug("Reading input file {}", file);
-	if (infile.is_open()) {
-		// first line -  grid size 
-		// second line - tracks per channel
+  string line;
+  int grid;
+  int tracks_per_channel;
+  circuit* circ = nullptr;
+  ifstream infile (file);
+  spdlog::debug("Reading input file {}", file);
+  if (infile.is_open()) {
+    // first line -  grid size 
+    // second line - tracks per channel
 
-		getline(infile, line);
-		grid = stoi(line);
+    getline(infile, line);
+    grid = stoi(line);
 
-		getline(infile, line);
-		tracks_per_channel = stoi(line);
+    getline(infile, line);
+    tracks_per_channel = stoi(line);
 
-		spdlog::debug("grid size {} tracks per channel {}", grid, tracks_per_channel);
-		circ = new circuit(grid, tracks_per_channel);
+    spdlog::debug("grid size {} tracks per channel {}", grid, tracks_per_channel);
+    circ = new circuit(grid, tracks_per_channel);
 
-		// read until we get the final line of all -1's
-		int x0 = 0, y0 = 0, p0 = 0, x1 = 0, y1 = 0, p1 = 0;
-		int all_neg1 = 0;
-		while (!all_neg1) {
-			infile >> x0;
-			infile >> y0;
-			infile >> p0;
-			infile >> x1;
-			infile >> y1;
-			infile >> p1;
-			spdlog::debug("read in x0 {} y0 {} p0 {} x1 {} y1 {} p1 {}", x0, y0, p0, x1, y1, p1);
+    // read until we get the final line of all -1's
+    int x0 = 0, y0 = 0, p0 = 0, x1 = 0, y1 = 0, p1 = 0;
+    int all_neg1 = 0;
+    while (!all_neg1) {
+      infile >> x0;
+      infile >> y0;
+      infile >> p0;
+      infile >> x1;
+      infile >> y1;
+      infile >> p1;
+      spdlog::debug("read in x0 {} y0 {} p0 {} x1 {} y1 {} p1 {}", x0, y0, p0, x1, y1, p1);
 
-			all_neg1 = (x0 == -1) && (y0 == -1) && (p0 == -1) && (x1 == -1) && (y1 == -1) && (p1 == -1);
-			if (!all_neg1) {
-				connection* conn = new connection(x0,y0,p0,x1,y1,p1);
-				circ->add_connection(conn);
-			}
-		}
-		string conns = circ->dump_connections();
-		spdlog::debug("connection dump:\n{}", conns);
+      all_neg1 = (x0 == -1) && (y0 == -1) && (p0 == -1) && (x1 == -1) && (y1 == -1) && (p1 == -1);
+      if (!all_neg1) {
+        connection* conn = new connection(x0,y0,p0,x1,y1,p1);
+        circ->add_connection(conn);
+      }
+    }
+    string conns = circ->dump_connections();
+    spdlog::debug("connection dump:\n{}", conns);
 
-		infile.close();
-	}
-	return circ;
+    infile.close();
+  }
+  return circ;
 }
 
 void print_usage() {
-	cout << "Usage: ./a1 [-h] [-d] -f circuit_file" << endl << endl;
+  cout << "Usage: ./a1 [-h] [-d] -f circuit_file" << endl << endl;
 }
 
 int main(int n, char** args) {
-	string file = "";
+  string file = "";
 
-	for(;;)
-	{
-		switch(getopt(n, args, "hf:d"))
-		{
-			case 'f':
-				file = optarg;
-				continue;
+  for(;;)
+  {
+    switch(getopt(n, args, "hf:d"))
+    {
+      case 'f':
+        file = optarg;
+        continue;
 
-			case 'd':
-				spdlog::set_level(spdlog::level::debug);
-				continue;
+      case 'd':
+        spdlog::set_level(spdlog::level::debug);
+        continue;
 
-			case '?':
-			case 'h':
-			default :
-				print_usage();
-				return 1;
+      case '?':
+      case 'h':
+      default :
+        print_usage();
+        return 1;
 
-			case -1:
-				break;
-		}
-		break;
-	}
+      case -1:
+        break;
+    }
+    break;
+  }
 
-	if (file == "") {
-		cerr << "Error: must provide input file" << endl;
-		print_usage();
-		return 1;
-	}
+  if (file == "") {
+    cerr << "Error: must provide input file" << endl;
+    print_usage();
+    return 1;
+  }
 
-	spdlog::info("a1 - Troy Denton 2023");
-	spdlog::info("Version {}.{}", VERSION_MAJOR, VERSION_MINOR);
-	spdlog::info("Commit {}", GIT_COMMIT);
-	spdlog::info("Built {}" , __TIMESTAMP__);
+  spdlog::info("a1 - Troy Denton 2023");
+  spdlog::info("Version {}.{}", VERSION_MAJOR, VERSION_MINOR);
+  spdlog::info("Commit {}", GIT_COMMIT);
+  spdlog::info("Built {}" , __TIMESTAMP__);
 
-	circuit* circ = read_input(file);
+  circuit* circ = read_input(file);
 
-	spdlog::info("Exiting");
-	delete(circ);
-	return 0;
+  spdlog::info("Exiting");
+  delete(circ);
+  return 0;
 }
