a6de25d922a3479a124bcf7e2977686b0300a7e6
Denton  Troy Ryan <dentontr@ug222.eecg.toronto.edu>
2023-09-30 19:15:38 -0400

    add test to see if ns and ew can be independent

--- switch_conn_test.cc
+++ switch_conn_test.cc
@@ -40,3 +40,16 @@ TEST(SwitchBlockTest, conn3) {
   ASSERT_TRUE(sb->is_connected(SOUTH,EAST,0));
   ASSERT_TRUE(sb->is_connected(WEST,EAST,0));
 }
+
+TEST(SwitchBlockTest, conn4) {
+  switch_block* sb = new switch_block(4);
+  
+  sb->connect(NORTH, SOUTH, 0);
+  sb->connect(EAST, WEST,0);
+  ASSERT_TRUE(sb->is_connected(NORTH,SOUTH,0));
+  ASSERT_FALSE(sb->is_connected(NORTH,EAST,0));
+  ASSERT_FALSE(sb->is_connected(NORTH,WEST,0));
+  ASSERT_FALSE(sb->is_connected(SOUTH,WEST,0));
+  ASSERT_FALSE(sb->is_connected(SOUTH,EAST,0));
+  ASSERT_TRUE(sb->is_connected(WEST,EAST,0));
+}
